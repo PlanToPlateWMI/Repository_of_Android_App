eeacb3daa145c933b7c82ebc95cdf180ec9f224c
Karyna Yarmosh <karyar@st.amu.edu.pl>
2023-04-25 13:38:02 +0200

    INZ-51 Add choice adult or child, new strings formats

---
+++ app/src/main/java/com/example/planetoplate_app/Choice_adult_or_child.java
@@ -0,0 +1,4 @@
+package com.example.planetoplate_app;
+
+public class Choice_adult_or_child {
+}
--- app/src/main/java/com/example/planetoplate_app/Register.java
+++ app/src/main/java/com/example/planetoplate_app/Register.java
@@ -9,6 +9,6 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.register_activity);
     }
 }
