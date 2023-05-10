cb8dc63e559e89f77bcb0b12081516fd5841ad12
Karyna Yarmosh <karyar@st.amu.edu.pl>
2023-05-10 14:23:07 +0200

    INZ-97 add search

--- app/src/main/java/pl/plantoplate/ui/main/BazaProduktowFragment.java
+++ app/src/main/java/pl/plantoplate/ui/main/BazaProduktowFragment.java
@@ -1,15 +1,20 @@
 package pl.plantoplate.ui.main;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.plantoplate.R;
 import pl.plantoplate.databinding.FragmentBazaProduktowBinding;
@@ -19,11 +24,29 @@ import pl.plantoplate.ui.main.shoplist.TrzebaKupicFragment;
 public class BazaProduktowFragment extends Fragment {
 
     private FragmentBazaProduktowBinding bazaProduktowBinding;
+    private SearchView searchView;
+    //this list will have items (products)
+    private  List<ClipData.Item> itemList;
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        searchView.findViewById(R.id.search);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterText(newText);
+                return true;
+            }
+        });
 
         bazaProduktowBinding = FragmentBazaProduktowBinding.inflate(inflater, container, false);
         replaceFragment(new WszystkieFragment());
@@ -41,6 +64,12 @@ public class BazaProduktowFragment extends Fragment {
         });
 
         return bazaProduktowBinding.getRoot();
+    }
+
+    private void filterText(String text) {
+        List<ClipData.Item> filteredList = new ArrayList<>();
+        for(ClipData.Item item : itemList){
+        }
     }
 
     private void replaceFragment(Fragment fragment) {
