53f818f08e1b2752794bae82501127901d464770
Karyna Yarmosh <karyar@st.amu.edu.pl>
2023-05-10 18:54:25 +0200

    INZ-100 add ToggleButtons to fragments - addYourOwnProduct and productChange, add fragment fragment_product_change

---
+++ app/src/main/java/pl/plantoplate/ui/main/ProductChangeFragment.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.plantoplate.ui.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pl.plantoplate.R;
+
+public class ProductChangeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_change, container, false);
+    }
+}
